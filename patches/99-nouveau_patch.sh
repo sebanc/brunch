54af9c4694a9d042d20c3752204da968a8ea299b
xeu100 <liamryde@gmail.com>
2020-05-26 20:49:38 -0400

    Create 99-nouveau_patch.sh

---
+++ patches/99-nouveau_patch.sh
@@ -0,0 +1,19 @@
+nouveau=0
+for i in $(echo "$1" | sed 's#,# #g')
+do
+        if [ "$i" == "nouveau" ]; then nouveau=1; fi
+done
+ 
+ret=0
+if [ "$nouveau" -eq 1 ]; then
+        echo "brunch: $0 nouveau enabled" > /dev/kmsg
+        cat >/system/etc/init/nouveau.conf <<MODPROBE
+start on stopped udev-trigger
+ 
+script
+        modprobe nouveau
+end script
+MODPROBE
+        if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 0))); fi
+fi
+exit $ret

dcf88053deb314370b4f5241b7e0b758bef610b2
Jaiden Nowak <windowsxp519@gmail.com>
2020-06-14 19:10:35 -0400

    Added GPU debugging

--- patches/99-features.sh
+++ patches/99-features.sh
@@ -18,10 +18,12 @@ echo '--ash-debug-shortcuts' >> /system/etc/chrome_dev.conf
 if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 1))); fi
 echo '--enable-features=ArcUsbStorageUI' >> /system/etc/chrome_dev.conf
 if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 2))); fi
-echo '--gpu-sandbox-failures-fatal=no' >> /system/etc/chrome_dev.conf
+echo '--enable-gpu-debugging' >> /system/etc/chrome_dev.conf
 if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 3))); fi
-echo '--enable-hardware-overlays="single-fullscreen,single-on-top"' >> /system/etc/chrome_dev.conf
+echo '--gpu-sandbox-failures-fatal=no' >> /system/etc/chrome_dev.conf
 if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 4))); fi
+echo '--enable-hardware-overlays="single-fullscreen,single-on-top"' >> /system/etc/chrome_dev.conf
+if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 5))); fi
 
 if [ "$acpi_power_button" -eq 1 ]; then
 	echo '--aura-legacy-power-button' >> /system/etc/chrome_dev.conf

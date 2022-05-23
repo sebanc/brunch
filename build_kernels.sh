8eaf66f1466ea5814e93b1a897d70fd38ba3f64d
sebanc <sebanc.xda@gmail.com>
2022-05-23 07:07:01 +0200

    Update build_kernels.sh

--- build_kernels.sh
+++ build_kernels.sh
@@ -125,7 +125,7 @@ build_kernels()
 kernels=$(ls -d ./kernels/* | sed 's#./kernels/##g')
 for kernel in $kernels; do
 	echo "Building kernel $kernel"
-	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make -C "./kernels/$kernel" -j$(($(nproc)-1)) O=out || { echo "Kernel build failed"; exit 1; }
+	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make -C "./kernels/$kernel" -j"$NTHREADS" O=out || { echo "Kernel build failed"; exit 1; }
 	if [ -f /persist/keys/brunch.priv ] && [ -f /persist/keys/brunch.pem ]; then
 		echo "Signing kernel $kernel"
 		mv "./kernels/$kernel/out/arch/x86/boot/bzImage" "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" || { echo "Kernel signing failed"; exit 1; }
@@ -155,48 +155,11 @@ for kernel in $kernels; do
 done
 }
 
-build_kernels_github()
-{
-kernels=$(ls -d ./kernels/* | sed 's#./kernels/##g')
-for kernel in $kernels; do
-	echo "Building kernel $kernel"
-	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make -C "./kernels/$kernel" -j4 O=out || { echo "Kernel build failed"; exit 1; }
-	if [ -f /persist/keys/brunch.priv ] && [ -f /persist/keys/brunch.pem ]; then
-		echo "Signing kernel $kernel"
-		mv "./kernels/$kernel/out/arch/x86/boot/bzImage" "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" || { echo "Kernel signing failed"; exit 1; }
-		sbsign --key /persist/keys/brunch.priv --cert /persist/keys/brunch.pem "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" --output "./kernels/$kernel/out/arch/x86/boot/bzImage" || { echo "Kernel signing failed"; exit 1; }
-	fi
-	echo "Including kernel $kernel headers"
-	srctree="./kernels/$kernel"
-	objtree="./kernels/$kernel/out"
-	SRCARCH="x86"
-	KCONFIG_CONFIG="$objtree/.config"
-	destdir="$srctree/headers"
-	(cd $srctree; find . -name Makefile\* -o -name Kconfig\* -o -name \*.pl) > "$objtree/hdrsrcfiles"
-	(cd $srctree; find arch/*/include include scripts -type f -o -type l) >> "$objtree/hdrsrcfiles"
-	(cd $srctree; find arch/$SRCARCH -name module.lds -o -name Kbuild.platforms -o -name Platform) >> "$objtree/hdrsrcfiles"
-	(cd $srctree; find $(find arch/$SRCARCH -name include -o -name scripts -type d) -type f) >> "$objtree/hdrsrcfiles"
-	if grep -q '^CONFIG_STACK_VALIDATION=y' $KCONFIG_CONFIG ; then
-		(cd $objtree; find tools/objtool -type f -executable) >> "$objtree/hdrobjfiles"
-	fi
-	(cd $objtree; find arch/$SRCARCH/include Module.symvers include scripts -type f) >> "$objtree/hdrobjfiles"
-	if grep -q '^CONFIG_GCC_PLUGINS=y' $KCONFIG_CONFIG ; then
-		(cd $objtree; find scripts/gcc-plugins -name \*.so -o -name gcc-common.h) >> "$objtree/hdrobjfiles"
-	fi
-	mkdir -p "$destdir"
-	(cd $srctree; tar -c -f - -T -) < "$objtree/hdrsrcfiles" | (cd $destdir; tar -xf -)
-	(cd $objtree; tar -c -f - -T -) < "$objtree/hdrobjfiles" | (cd $destdir; tar -xf -)
-	cp $objtree/.config $destdir/.config
-done
-}
+if [ -z "$RUNNER_TEMP" ]; then NTHREADS=$(($(nproc)-1)); else NTHREADS=4; fi
 
 rm -rf ./kernels
 mkdir ./kernels
 
 chromeos_version="R103"
 download_and_patch_kernels
-if [ ! "$1" == "github-actions" ]; then
 build_kernels
-else
-build_kernels_github
-fi

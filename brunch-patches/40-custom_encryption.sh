fa902a3114ccc9f020fd8c906dbb97d28d4f5961
sebanc <sebanc.xda@gmail.com>
2022-07-20 06:27:52 +0200

    Changelog: - Attempt to fix an encryption bug in the previous build.
    Warning: due to a change in cryptohome all your data will be lost if you update to this build. Make sure you backup your data and perform a clean brunch install.

--- brunch-patches/40-custom_encryption.sh
+++ brunch-patches/40-custom_encryption.sh
@@ -4,10 +4,10 @@ ret=0
 
 cat >/roota/usr/share/cros/startup_utils.sh <<'MOUNTS'
 mount_var_and_home_chronos() {
-  mkdir -p /mnt/stateful_partition/var_overlay
-  mount -n --bind /mnt/stateful_partition/var_overlay /var || return 1
-  mkdir -p /mnt/stateful_partition/home/chronos
-  mount -n --bind /mnt/stateful_partition/home/chronos /home/chronos || return 1
+  mkdir -p /mnt/stateful_partition/brunch/data/var
+  mount -n --bind /mnt/stateful_partition/brunch/data/var /var || return 1
+  mkdir -p /mnt/stateful_partition/brunch/data/home/chronos
+  mount -n --bind /mnt/stateful_partition/brunch/data/home/chronos /home/chronos || return 1
 }
 
 umount_var_and_home_chronos() {

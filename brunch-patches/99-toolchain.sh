bb733233c159f59a208eb6de3105f7b0e65074b7
sebanc <sebanc.xda@gmail.com>
2022-11-17 20:51:09 +0100

    Changelog: - Support for identity v4 - Minor bugfix

--- brunch-patches/99-toolchain.sh
+++ brunch-patches/99-toolchain.sh
@@ -2,7 +2,7 @@
 
 for i in /roota/usr/lib64/libm.so /roota/usr/lib64/libpthread.so /roota/usr/lib64/libc.so
 do
-	if grep -q 'OUTPUT_FORMAT' $i; then sed -i "s@/usr/lib64/@/usr/local/lib64/@g" $i; fi
+	if [ -f "$i" ] && grep -q 'OUTPUT_FORMAT' "$i"; then sed -i "s@/usr/lib64/@/usr/local/lib64/@g" $i; fi
 done
 
 sed -i 's@MODE="0660"@MODE="0666"@g' /roota/lib/udev/rules.d/99-vm.rules

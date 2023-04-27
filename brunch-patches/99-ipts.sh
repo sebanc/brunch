6bd6e935e71505bdd124dc40e999b9b40f554cb4
sebanc <sebanc.xda@gmail.com>
2023-04-27 07:33:14 +0200

    Changelog: - Review iptsd implementation

--- brunch-patches/99-ipts.sh
+++ brunch-patches/99-ipts.sh
@@ -9,17 +9,30 @@ done
 ret=0
 if [ "$ipts" -eq 1 ]; then
 	echo "brunch: $0 ipts enabled" > /dev/kmsg
-	cat >/roota/etc/init/ipts.conf <<IPTS
+	if [ ! "$(cat /proc/version | cut -d' ' -f3 | cut -c1-4 | sed 's@\.@@g')" -eq 510 ] || [ ! "$(cat /proc/version | cut -d' ' -f3 | cut -c1-4 | sed 's@\.@@g')" -eq 515 ]; then
+		cat >/roota/etc/init/ipts.conf <<IPTS
 start on stopped udev-trigger
-
+script
+	insmod /lib/modules/$(cat /proc/version |  cut -d' ' -f3)/ipts.ko
+	insmod /lib/modules/$(cat /proc/version |  cut -d' ' -f3)/ithc.ko
+	iptsd
+end script
+IPTS
+		if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 0))); fi
+		tar zxf /rootc/packages/ipts-old.tar.gz -C /roota
+		if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 1))); fi
+	else
+		cat >/roota/etc/init/ipts.conf <<IPTS
+start on stopped udev-trigger
 script
 	insmod /lib/modules/$(cat /proc/version |  cut -d' ' -f3)/ipts.ko
 	insmod /lib/modules/$(cat /proc/version |  cut -d' ' -f3)/ithc.ko
 	iptsd \$(iptsd-find-hidraw)
 end script
 IPTS
-	if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 0))); fi
-	tar zxf /rootc/packages/ipts.tar.gz -C /roota
-	if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 1))); fi
+		if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 2))); fi
+		tar zxf /rootc/packages/ipts-new.tar.gz -C /roota
+		if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 3))); fi
+	fi
 fi
 exit $ret

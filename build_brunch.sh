2476c0dee39642d0dbc0f2e8e3bf28e27b6f59a5
sebanc <sebanc.xda@gmail.com>
2024-03-17 06:47:57 +0100

    Changelog: - Review firmwares install commands

--- build_brunch.sh
+++ build_brunch.sh
@@ -318,8 +318,8 @@ curl -L https://archlinux.org/packages/extra/any/intel-ucode/download/ -o /tmp/i
 tar -C ../rootc/lib/firmware/ -xf /tmp/intel-ucode.tar.zst boot/intel-ucode.img --strip 1 || { echo "Failed to extract intel ucode"; exit 1; }
 rm /tmp/intel-ucode.tar.zst || { echo "Failed to cleanup intel ucode"; exit 1; }
 cd ./out || { echo "Failed to enter the final firmware directory"; exit 1; }
+find . -type l -not -name '*.xz' -exec bash -c 't=$(readlink "{}"); if [ -f "$(realpath $(readlink -f ${t}))" ]; then ln -sf "${t}".xz "{}".xz; rm "{}"; fi' \;
 find . -type f -not -name '*.xz' -exec xz -C crc32 '{}' \;
-find . -type l -not -name '*.xz' -exec bash -c 't=$(readlink "{}"); if [ ! -d "${t}" ]; then ln -sf "${t}".xz "{}".xz; rm "{}"; fi' \;
 tar zcf ../../rootc/packages/firmwares.tar.gz * --owner=0 --group=0 || { echo "Failed to create the firmwares archive"; exit 1; }
 cd ../.. || { echo "Failed to cleanup firmwares directory"; exit 1; }
 rm -r ./linux-firmware || { echo "Failed to cleanup firmwares directory"; exit 1; }

c59e3b46f77bfeda530c9139994940a9f0d7d0b5
sebanc <sebanc.xda@gmail.com>
2024-06-29 20:16:40 +0200

    Changelog: - Build Brunch against ChromeOS r126

--- build_brunch.sh
+++ build_brunch.sh
@@ -32,7 +32,7 @@ if [ ! -z $1 ] && [ "$1" != "skip" ] ; then
 else
 	git clone -b master https://github.com/sebanc/chromeos-ota-extract.git rootfs || { echo "Failed to clone chromeos-ota-extract"; exit 1; }
 	cd rootfs
-	curl -L https://dl.google.com/chromeos/rammus/15853.53.0/stable-channel/chromeos_15853.53.0_rammus_stable-channel_full_mp-v4.bin-gy3dkntdhbtdsfmfbmsfr2mmz5x3ynoy.signed -o ./update.signed || { echo "Failed to Download the OTA update"; exit 1; }
+	curl -L https://dl.google.com/chromeos/rammus/15886.44.0/stable-channel/chromeos_15886.44.0_rammus_stable-channel_full_mp-v5.bin-gy3doojqmq2dnanekinnjrzp4emrw6f2.signed -o ./update.signed || { echo "Failed to Download the OTA update"; exit 1; }
 	python3 extract_android_ota_payload.py ./update.signed || { echo "Failed to extract the OTA update"; exit 1; }
 	cd ..
 	[ -f ./rootfs/root.img ] || { echo "ChromeOS rootfs has not been extracted"; exit 1; }

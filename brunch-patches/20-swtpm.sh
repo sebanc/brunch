bcf51ebc6cc95ce73281fad4bf6464477a922329
sebanc <sebanc.xda@gmail.com>
2024-09-07 15:31:50 +0200

    Re-enable stateful encryption

--- brunch-patches/20-swtpm.sh
+++ brunch-patches/20-swtpm.sh
@@ -5,8 +5,6 @@ ret=0
 cat >/roota/etc/init/tpm2-simulator.conf <<'SWTPM'
 stop on stopping boot-services and stopped trunksd
 
-# This daemon should very unlikely to be killed by the OOM killer otherwise
-# the other TPM related daemons(trunksd/chapsd/cryptohomed...) may crash.
 oom score -100
 
 respawn
@@ -21,16 +19,9 @@ exec /usr/bin/swtpm chardev --daemon --vtpm-proxy --tpm2 --tpmstate dir=/mnt/sta
 
 post-start exec bash -c "until [ -c /dev/tpm0 ]; do sleep 1; done"
 
-# Add timeout and sync the disk to prevent blocking the shutdown.
-pre-stop script
-sync
-end script
-
 post-stop script
-# Kill the remaining simulator processes.
-pgrep swtpm | xargs -r kill
-# Sync the disk to prevent losing data.
-sync
+  pgrep swtpm | xargs -r kill
+  sync
 end script
 SWTPM
 if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 0))); fi
--- build_brunch.sh
+++ build_brunch.sh
@@ -32,7 +32,7 @@ if [ ! -z $1 ] && [ "$1" != "skip" ] ; then
 else
 	git clone -b master https://github.com/sebanc/chromeos-ota-extract.git rootfs || { echo "Failed to clone chromeos-ota-extract"; exit 1; }
 	cd rootfs
-	curl -L https://dl.google.com/chromeos/rammus/15917.65.0/stable-channel/chromeos_15917.65.0_rammus_stable-channel_full_mp-v5.bin-gy3geolegq2dtlj74bjj2vzlxun552cf.signed -o ./update.signed || { echo "Failed to Download the OTA update"; exit 1; }
+	curl -L https://dl.google.com/chromeos/rammus/15964.41.0/stable-channel/chromeos_15964.41.0_rammus_stable-channel_full_mp-v5.bin-gy3gimjwmi2tmqhcsd6tt536d3o573kf.signed -o ./update.signed || { echo "Failed to Download the OTA update"; exit 1; }
 	python3 extract_android_ota_payload.py ./update.signed || { echo "Failed to extract the OTA update"; exit 1; }
 	cd ..
 	[ -f ./rootfs/root.img ] || { echo "ChromeOS rootfs has not been extracted"; exit 1; }
